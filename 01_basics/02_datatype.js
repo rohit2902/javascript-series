db635ad30e5ddea84260888c65972d8ccd4e3f50
rohit2902 <rohitabrssm@gmail.com>
2024-07-11 12:48:25 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) //we are using node.js, not browser
+
+console.log(3+3);
+ console.log("Rohit")
+
+ let name ="rohit"
+ let age = 18
+ let isloggedin = false
+
+ //number =>2 to power 53
+ //bigint
+ //string
+ //boolean
+ //null => standalone value
+ //undefined
+ //symbol => unique
+
+ //object
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null); //object
