bed2e95ed54cffc59758aead9fca89ffc38df821
rohit2902 <rohitabrssm@gmail.com>
2024-07-11 13:07:42 +0000

    conversion

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,14 @@
+let score = "33"
+// let scor = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNUmber = Number(score)
+console.log(typeof valueInNUmber)
+console.log(typeof(valueInNUmber))
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
