76358821e21d6e3a720a9deb615888d0ca0e0a28
rohit2902 <rohitabrssm@gmail.com>
2024-07-11 13:36:57 +0000

    operation

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -1,14 +1,31 @@
-let score = "33"
+//let score = "33"
 // let scor = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNUmber = Number(score)
-console.log(typeof valueInNUmber)
-console.log(typeof(valueInNUmber))
+// let valueInNUmber = Number(score)
+// console.log(typeof valueInNUmber)
+// console.log(typeof(valueInNUmber))
 
 
 //"33" => 33
 //"33abc" => NaN
 //true => 1; false =>0
+
+//**********************Operation********** */
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(10%3)
+
+let str1 = "hello"
+let str2 = " rohit"
+let str3 = str1+str2
+console.log(str3)
