0a061fbbe333e597eb13ac7fae2bbad8533dbbd4
rohit2902 <rohitabrssm@gmail.com>
2024-07-11 12:47:58 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId =7781
+let accountEmail = "rohitssm@gmail.com"
+var accountPassword = "12345"
+accountCity = "sasaram"
+let accountstate
+
+// accountId = 2  // not allowed
+
+accountEmail ="rohit@gmail"
+accountPassword ="789456"
+accountCity ="dehri"
+
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
