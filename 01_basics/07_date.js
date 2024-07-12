4c4c0383610bcbb2444b44a73b9a282673024072
rohit2902 <rohitabrssm@gmail.com>
2024-07-12 13:00:54 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,21 @@
+// DAtes
+
+let MyDate = new Date()
+// console.log(MyDate.toString())
+// console.log(MyDate.toDateString())
+// console.log(MyDate.toLocaleString())
+// console.log(MyDate.toISOString())
+// console.log(MyDate.toLocaleTimeString())
+
+//let mycreatedate = new Date(2023, 0 ,5)
+//let mycreatedate = new Date(2023, 0 ,23,5,3)
+//let mycreatedate = new Date("2023-01-14")
+let mycreatedate = new Date("01-14-2023")
+
+
+//console.log(mycreatedate.toLocaleString())
+
+let mytimestamp = Date.now()
+
+console.log(mytimestamp)
+console.log(mycreatedate.getTime())
