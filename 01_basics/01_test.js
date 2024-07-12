c3ac90d85057388f870d66951aefe2a9389a1ff2
rohit2902 <rohitabrssm@gmail.com>
2024-07-12 13:01:19 +0000

    test

--- 01_basics/01_test.js
+++ 01_basics/01_test.js
@@ -1,1 +1,9 @@
 console.log("Rohit")
+// DAtes
+
+let MyDate = new Date()
+console.log(MyDate.toString())
+console.log(MyDate.toDateString())
+console.log(MyDate.toLocaleString())
+console.log(MyDate.toISOString())
+console.log(MyDate.toLocaleTimeString())
