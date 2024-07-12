a063ae333976adc9f122dcff134d619fe876cdab
rohit2902 <rohitabrssm@gmail.com>
2024-07-12 14:07:59 +0000

    array

---
+++ 02_basics/20_array.js
@@ -0,0 +1,39 @@
+const myarr = [1,2,3,4,5,6,7,8]
+const hero = ["shaktiman" , "naagraj"]
+ //console.log(hero[0])
+
+ const myarr2 = new Array(1,2,3,4,5)
+ //console.log(myarr2)
+
+ // Array Method
+//  myarr.push(12) //add element
+//  myarr.push(70)
+//  myarr.pop()  // delete element from lest
+//  myarr.unshift(11) //add new element from staring array
+//  myarr.shift()  // remove from first
+//  console.log(myarr)
+//  console.log(myarr.includes(9))
+//  console.log(myarr.indexOf(3))
+
+// console.log("A" ,myarr)
+
+// const myn1 = myarr.slice(1,3)
+// console.log(myn1)
+// console.log("B" ,myn1)
+
+// const myn2 = myarr.splice(1,3)
+// console.log("c" ,myarr)
+// console.log(myn2)
+
+
+// Part 2
+
+const marvel_heros =["thor","Ironman","spiderman"]
+const dc = ["superman","flash","batman"]
+// marvel_heros.push(dc)
+// console.log(marvel_heros)
+
+//  const all_hero = marvel_heros.concat(dc)
+// console.log(all_hero)
+
+
