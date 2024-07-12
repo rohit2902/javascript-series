d966f0e0b63766d6303fbf24f5e7128da148a5de
rohit2902 <rohitabrssm@gmail.com>
2024-07-12 15:09:03 +0000

    object

---
+++ 02_basics/01_object.js
@@ -0,0 +1,60 @@
+// singleton
+//object.create
+
+// object literals
+
+// const mysym = Symbol("key1")
+
+// const jsUser =
+// {
+//     Name: "rohit",
+//     "full name ": "rohit kumar",
+//     [mysym]: "mykey1",
+//     age: 18,
+//     location: "sasaram",
+//     email: "rohitabr@gmail.com",
+//     isLoggedIn: false,
+//     lastLogindays:["Monday, saturday"]
+
+// }
+
+// console.log(jsUser.email)
+// console.log(jsUser["email"])
+// console.log(jsUser["full name "])
+// console.log(jsUser[mysym])
+
+
+// part2 
+
+// const tinderUser = new object()
+const tinderUser = {}
+tinderUser.id ="123asd"
+tinderUser.name = "rohit"
+tinderUser.isloggedIn = false
+
+//console.log(tinderUser)
+
+const regularuser ={
+    email:"rohit@gmail",
+    fullname:{
+        userfullname:{
+           firstname:"rohit",
+           lastname:"kumar"
+        }
+    }
+}
+//console.log(regularuser.fullname)
+
+ const obj1 ={1:"a",2:"b"}
+ const obj2={3:"d",4:"e"}
+
+ //const obj3 ={obj1,obj2}
+ //const obj3 = Object.assign(obj1,obj2)
+ const obj3 = {...obj1,...obj2}
+ //console.log(obj3)
+
+ console.log(tinderUser)
+ console.log(Object.keys(tinderUser))
+ console.log(Object.values(tinderUser))
+ console.log(Object.entries(tinderUser))
+ console.log(tinderUser.hasOwnProperty('isLoggedIn'))
