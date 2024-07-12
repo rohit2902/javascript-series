ecebbe6e35d27cfcacd72a5b441d9706b5b8ecf9
rohit2902 <rohitabrssm@gmail.com>
2024-07-12 10:54:42 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name ="rohit"
+const repocount = 50
+const gameName = new String(`    Rohit      kumar   `)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+// const newstring = gameName.substring(0,4)
+// console.log(newstring)
+
+// const anotherstring = gameName.slice(-8,4)
+// console.log(anotherstring)
+console.log(gameName.trim())
+
+const url ="https://rohit.com/rohit%2kumar"
+console.log(url.replace('%2',' '))
+console.log(url.includes('rohit'))
+console.log(url.includes('sunder'))
