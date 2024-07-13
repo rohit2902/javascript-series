1308c95a02d8ea1bc50e819cd652a9671f0f89d7
rohit2902 <rohitabrssm@gmail.com>
2024-07-13 05:34:53 +0000

    js chalenge

---
+++ 30_day_js/01_day_variable.js
@@ -0,0 +1,53 @@
+ //Activity 1
+
+ // task 1
+var num1 = 12345
+//console.log(`the number is ${num1}`)
+//console.log(typeof num1)
+
+//Task 2:
+let Name = "Rohit"
+//console.log(`My name is ${Name}`)
+//console.log(typeof str1)
+
+// Activity 2
+
+//  Task 3
+
+const islogIn = true
+//console.log(typeof islogIn)
+//console.log(`islogIn ? ${islogIn}`)
+
+//Activity 3
+// Task 4
+let num2 = 1234
+//console.log(typeof num2)
+// let full_name = "Rohit kumar"
+// console.log(typeof full_name)
+
+// const isLogIn = false
+// console.log(typeof isLogIn)
+
+// let user ={
+//     Nmae: "Rohit",
+//     Email: "Rohit@gmail.com"
+// }
+// console.log(typeof user)
+
+// const myarr = new Array(1,2,3,4)
+// const myarr2 =[1,2,3,4]
+// console.log(typeof myarr2)
+
+let Name1 = "rohit"
+let full_name1 = Name1
+ full_name1 = "Rohit kumar"
+console.log(Name1)
+console.log(full_name1)
+
+const myyoutubename ="rohitkumar@123"
+const anothername = myyoutubename
+ anothername ="kushwaha" // assignment to constant variable
+
+console.log(anothername)
+console.log(myyoutubename)
+
