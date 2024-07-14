b1684b123f97e6fc6076c3af224d84e0032163df
rohit2902 <rohitabrssm@gmail.com>
2024-07-14 05:41:25 +0000

    02_day_challenge

---
+++ 30_day_js/02_day_operator.js
@@ -0,0 +1,69 @@
+//Arithmetic operator
+
+// task 1
+
+// let num1 = 10
+// let num2 = 20
+// let result =num1+num2
+// console.log(`The sum of two number is ${result}`)
+
+// let num1 = 10
+// let num2 = 20
+// let result =num1-num2
+// console.log(`The Subtract of two number is ${result}`)
+
+//let num1 = 10
+// let num2 = 20
+// let result =num1*num2
+// console.log(`The multiply of two number is ${result}`)
+
+// let num1 = 10
+// let num2 = 20
+// let result =num1/num2
+// console.log(`The divide of two number is ${result}`)
+
+// let num1 = 10
+// let num2 = 20
+// let result =num1%num2
+// console.log(`The remainder of two number is ${result}`)
+
+// Activity 2
+
+//Assignment operator
+
+//task 1
+//  let score = 10
+//  score +=5
+//  console.log(`the score is ${score}`)
+
+//  // Task 2
+
+//  let score1 = 10
+//  score1 -=5
+//  console.log(`the score is ${score1}`)
+
+//Activity 3
+
+// task 
+
+let score3 = 10
+let score4 =20
+// console.log( score3 > score4)
+// console.log(score3 < score4)
+
+let score5 = 10
+//let score6 =20
+// console.log( score5 >= score6)
+// console.log(score5 <= score6)
+
+// console.log( score5 == score6)
+// console.log(score5 === score6)
+
+// console.log((score6 >=20)&& (score6<=20))
+// console.log((score6 >=20)|| (score6<=20))
+// console.log((score6 >=20)!= (score6<=20))
+let score6 =20
+    score5>=20 ?console.log("yes"):console.log("no")
+
+
+
