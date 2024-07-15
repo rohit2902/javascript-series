11e15b3056640862a4c9194c9af462a2c1d8c93b
rohit2902 <rohitabrssm@gmail.com>
2024-07-15 13:20:58 +0000

    day 03 challenge completed

---
+++ 30_day_js/03_day_control.js
@@ -0,0 +1,89 @@
+// const num1 = 10
+// if (num1>0) {
+//     console.log(`${num1}  is positive number `)
+// }
+// else if (num1==0) {
+//     console.log(`${num1}  is  Zero number  `)
+// } else {
+//     console.log(`${num1}  is negative number `)
+// }
+
+// let age = 18
+// if (age>18) {
+//     console.log("You can vote")
+// }
+// else{
+//     console.log("You can not vote")
+// }
+
+// let num1 = 10
+// let num2 = 20
+// let num3 = 30
+// if (num1>num2 && num1>num3) {
+//     console.log("A is largest Number")
+// }
+// if (num2>num3) {
+//     console.log("B is largest number")
+// } else {
+//     console.log("C is largest")
+    
+// }
+
+// const num1 = 2
+// switch (num1) {
+//     case 1:
+//         console.log("Sunday")
+//         break;
+//     case 2:
+//         console.log("Monday")    
+//         break;
+//     case 3:
+//         console.log("tuesday")
+//         break;
+//     case 4:
+//         console.log("Wednesday")
+//         break;
+//     case 5:
+//         console.log("Thursday")
+//         break;
+//     case 6:
+//         console.log("Friday")
+//         break;                
+//     default:
+//         console.log("saturday")
+//         break;
+// }
+ //let score = 60
+// switch (score) {
+//     case 90:
+//         console.log("A")
+//         break;
+//     case 80:
+//         console.log("B")
+//         break;
+//     case 70:
+//         console.log("C")  
+//         break;
+//     case 60:
+//         console.log("D") 
+//         break;     
+//     default:
+//         console.log("Fail")
+//         break;
+// }
+
+let num1 = 4
+// if (num1%2==0) {
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+
+let year = 2004
+if ((year%4==0) && (year%100!=0)||(year%400==0) ) {
+    console.log(" Leap year")
+}
+else{
+    console.log(" Not Leap year")
+}
