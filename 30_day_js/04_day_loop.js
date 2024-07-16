24e19ee6ec3e747ee7fb6a39e11185c47738c7c5
rohit2902 <rohitabrssm@gmail.com>
2024-07-16 11:47:36 +0000

    day 4

---
+++ 30_day_js/04_day_loop.js
@@ -0,0 +1,69 @@
+// for (let i = 1; i <=10;i++){
+//     console.log(i ," ")
+// }
+// let num1 = 5
+// for (let i = 1; i <=10;i++){
+//     console.log(num1,"*",i ,"=",num1*i)
+
+
+// }
+// let n= 10
+// let i = 1
+// let sum = 0
+// while(i<=n){
+//     i++
+//     sum = sum+i
+// }
+// console.log(sum)
+
+
+// let i = 10
+// while (i>=1) {
+//     console.log(i," ")
+//     i--
+// }
+
+// let i =1
+// do{
+//     console.log(i)
+//     i++
+// }
+//     while(i<=5)
+
+
+// let n = 5
+// let i = 1
+// let fac = 1
+// do{
+//     fac = fac*i
+//     i++
+// }
+// while(i<=5)
+//     console.log(fac)
+
+
+// nested loop 
+// let line = 5
+// for(let i = 1; i<=line;i++){
+//     for(let j = 1;j<=i;j++){
+//         console.log("*", " ");
+//     } 
+   
+// }
+
+for(let i = 1;i<=10;i++){
+    if(i==5){
+       break;
+    }
+    console.log(i)
+}
+
+for(let i = 1;i<=10;i++){
+    if(i==5){
+       continue;
+    }
+    console.log(i)
+}
+
+
+
