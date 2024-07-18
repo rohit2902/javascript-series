413dd5a263e154860aca997df70578f9c3ba415b
rohit2902 <rohitabrssm@gmail.com>
2024-07-18 13:16:07 +0000

    array

---
+++ 30_day_js/05_day_array.js
@@ -0,0 +1,45 @@
+//let score =[1,2,3,4,5,6,7,8,9,10]
+// console.log(score)
+// console.log(score[0])
+// console.log(score[score.length-1])
+//score.pop()
+//console.log(score)  /// delete an element from end
+//score.push(12)
+//console.log(score)   // add new element from end
+
+//score.shift()           // delete an element from start
+//console.log(score)
+
+//score.unshift(0)
+//console.log(score)
+
+// const add_number = score.map((num)=>num+10)
+// console.log(add_number)
+
+
+// const checkodd_even =score.filter((num) => {
+//     return num%2==0
+// })
+//console.log(checkodd_even)
+
+// let score =[1,2,3,4]
+// const totsum = score.reduce(function (curr,sum) {
+//     return curr+sum
+// })
+
+// console.log(totsum)
+
+ let matrix =[
+     [1,2],
+     [5,6]
+ ]
+ console.log(matrix[1,2])
+
+let marks =[74,85,75,76,99,97]
+// for(let  id = 0;id<marks.length;id++){
+//     console.log(marks[id])
+// }
+
+// marks.forEach(element => {
+//     console.log(element)
+// });
