39ab5a317d5fd2e9c9ecdb871ff0cab56c3388b4
rohit2902 <rohitabrssm@gmail.com>
2024-07-19 08:57:17 +0000

    day 7 complete

---
+++ 30_day_js/07_day_object.js
@@ -0,0 +1,16 @@
+const book ={
+    title:"let us c programming",
+    author:"Yashavant Kanetkar",
+    year:"2002",
+    function () {
+        
+        
+        return `${this.title} and ${this.author}`
+    }
+}
+// console.log(book)
+// console.log(book.title)
+// console.log(book.author)
+
+//st()
+console.log(`${this}`)
