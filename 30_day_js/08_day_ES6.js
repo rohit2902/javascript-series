2a6badf02b9600d6bb28299e38222efd7500c7cf
rohit2902 <rohitabrssm@gmail.com>
2024-07-20 08:30:30 +0000

    day 8 completed

---
+++ 30_day_js/08_day_ES6.js
@@ -0,0 +1,66 @@
+// let name = "rohit kumar"
+// let age = 19
+
+// const LogIn = (`My name is ${name} and age is ${age}`)
+// console.log(LogIn)
+
+
+// task 2
+
+// const person =(` my 
+//                    name 
+//                         is 
+//                           rohit
+                                
+//     `)
+//     console.log(person)
+
+// destructuring
+
+// const arr1 =[1,2,3,4,5,6,7,8]
+// const [a,b] =arr1
+// console.log(a)
+// console.log(b)
+
+// const obj ={ title:"Let us c",Author:"Yashavant Kanetkar",year:2004}
+// const{title,Author} =obj
+// console.log(title)
+// console.log(Author)
+
+// let arr1 =[1,2,3,4,5,6]
+// const arr2 =[7,8,9,10]
+// arr1 =[...arr1,...arr2]
+// console.log(arr1)
+
+// let arr1 =[1,2,3,4,5,6]
+// function sum(x,y,z,...rest){
+//     return x+y+z
+// }
+// console.log(sum(...arr1))
+
+// const num1 = 10
+// const num2 = 5
+// function pro(num1,num2 =2){
+//     return num1*num2
+// }
+// console.log(pro(10))
+
+// const person ={
+//     name:"rohit kumar",
+//     age:20,
+//     address:"sasaram",
+//     function(){
+//         console.log("BCA student")
+//     }
+// }
+// console.log(person)
+
+const person ={
+     let "name":"rohit kumar",
+    age:20,
+    address:"sasaram",
+    function(){
+        console.log("BCA student")
+    }
+}
+console.log(person)
